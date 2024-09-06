92a26604e315c2d3a03147450670a0d8eeb4e54d
Elton Kaqiu <eltonkaqiu.github@gmail.com>
2024-09-06 11:44:25 +0200

    created a response model to use for Api response

--- src/main/java/com/totty/shoppingcart/models/dto/ImageDto.java
+++ src/main/java/com/totty/shoppingcart/models/dto/ImageDto.java
@@ -1,8 +1,12 @@
 package com.totty.shoppingcart.models.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ImageDto {
     private Long imageId;
     private String imageName;
--- src/main/java/com/totty/shoppingcart/models/response/ApiResponse.java
+++ src/main/java/com/totty/shoppingcart/models/response/ApiResponse.java
@@ -1,4 +1,21 @@
 package com.totty.shoppingcart.models.response;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private String message;
+    private int statusCode;
+    private boolean success;
+    private T data;
+    private List<String> errors;
+    private String path;
+    private LocalDateTime timestamp;
 }
